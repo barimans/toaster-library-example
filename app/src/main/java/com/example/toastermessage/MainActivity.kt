75d1a292b1dc19aadc4cac425c1eb8af4e40ea51
barimanS <bariman.s@bts.id>
2020-02-20 10:33:30 +0700

    initial commit

---
+++ app/src/main/java/com/example/toastermessage/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.toastermessage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ toaterlibrary/src/main/java/com/example/toaterlibrary/ToasterMessage.kt
@@ -0,0 +1,15 @@
+package com.example.toaterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+
+
+class ToasterMessage {
+
+    fun show(c: Context, message: String) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+
+    }
+}
